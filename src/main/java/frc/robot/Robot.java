d3359d65c05a4dd5a388e77015bb57d69993272b
bernardo almeida <bernardothesecond@gmail.com>
2024-09-25 18:43:20 -0300

    fixed stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,12 +3,14 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.units.Velocity;
 import edu.wpi.first.wpilibj.Joystick;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.InvertType;
 
 public class Robot extends TimedRobot {
-  
+
   private VictorSPX right_1 = new VictorSPX(2);
   private VictorSPX right_2 = new VictorSPX(1); 
   private VictorSPX left_1 = new VictorSPX(4); 
@@ -16,13 +18,17 @@ public class Robot extends TimedRobot {
 
   private Joystick joy = new Joystick(0);
   private double Px,Py,Px2,Py2,rad, LSpeed, RSpeed, TriggerValue, diff, mag;
-  private double velocity = 1;
+  private double velocity = 0.25;
   private boolean a,b,x;
   private int quad, POV;
   
   private double Deadzone(double Value) {
-    if (Math.abs(Value) < 0.04) return 0;
-    else return Value;
+    if (Math.abs(Value) < 0.04) {
+      return 0;
+    }
+    else {
+      return Value;
+    }
   }
 
   private int getQuad(double X, double Y) {
@@ -45,27 +51,31 @@ public class Robot extends TimedRobot {
       case 2: LSpeed = diff; RSpeed = mag + diff; break;
       case 3: LSpeed = diff * -1; RSpeed = (mag + diff) * -1; break;
       case 4: RSpeed = diff * -1; LSpeed = (mag + diff) * -1; break;
-      default: RSpeed = LSpeed = 0; break;
+      default: LSpeed = RSpeed = 0; break;
     }
 
-    if (Px > 0 && Py == 0) {
-      RSpeed = 0; LSpeed = 1;
-    } 
-    if (Px < 0 && Py == 0) {
-      RSpeed = 1; LSpeed = 0;
-    }
+    if (Px < 0 && Py == 0) {RSpeed = mag; LSpeed = -mag;}
+    if (Px > 0 && Py == 0) {LSpeed = mag; RSpeed = -mag;}
 
-    if (Px == 0 && Py > 0) LSpeed = RSpeed = 1;
-    if (Px == 0 && Py < 0) LSpeed = RSpeed = -1;
+    if (Px == 0 && Py != 0) {
+        LSpeed = RSpeed = mag * Math.sin(rad);
+    }
 
     if (TriggerValue !=0 && (Px == 0 && Py == 0)) {
       LSpeed = RSpeed += TriggerValue;
     } else if (TriggerValue !=0 && (Px != 0 && Py != 0)) {
       LSpeed = RSpeed *= TriggerValue;
-    } 
+    }
+  }
 
-    LSpeed = Math.max(-1, Math.min(1, LSpeed)) * velocity;
-    RSpeed = Math.max(-1, Math.min(1, RSpeed)) * velocity;
+  public void SetVelocity() {
+    a = joy.getRawButton(1);
+    b = joy.getRawButton(2);
+    x = joy.getRawButton(3);
+
+    if (b) {velocity = 0.25;}
+    if (a) {velocity = 0.5;}
+    if (x) {velocity = 1;}
   }
 
   @Override
@@ -73,11 +83,7 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void robotPeriodic() {
-    SmartDashboard.putNumber("Radiano", rad);
-    SmartDashboard.putNumber("Esq", LSpeed);
-    SmartDashboard.putNumber("Dir", RSpeed);
-  }
+  public void robotPeriodic() {}
   
   @Override
   public void teleopInit() {
@@ -92,13 +98,15 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+    
     POV = joy.getPOV();
+
     Px = Deadzone(joy.getRawAxis(0));
     Py = Deadzone(-joy.getRawAxis(1));
     Px2 = Deadzone(-joy.getRawAxis(4));
     Py2 = Deadzone(joy.getRawAxis(5));
     
-    TriggerValue = joy.getRawAxis(2) - joy.getRawAxis(3);
+    TriggerValue = joy.getRawAxis(3) - joy.getRawAxis(2);
 
     if (POV != -1) {
       pov();
@@ -110,16 +118,17 @@ public class Robot extends TimedRobot {
       CalculateSpeed(Px2, Py2);
     }
 
-    a = joy.getRawButton(1);
-    b = joy.getRawButton(2);
-    x = joy.getRawButton(3);
+    SetVelocity();
 
-    if (b) velocity = 0.25;
-    if (a) velocity = 0.5;
-    if (x) velocity = 1;
+    LSpeed = Math.max(-1, Math.min(1, LSpeed)) * velocity;
+    RSpeed = Math.max(-1, Math.min(1, RSpeed)) * velocity;
 
     right_1.set(ControlMode.PercentOutput, RSpeed);
     left_1.set(ControlMode.PercentOutput, LSpeed);
+
+    SmartDashboard.putNumber("Radiano", rad);
+    SmartDashboard.putNumber("Esq", LSpeed);
+    SmartDashboard.putNumber("Dir", RSpeed);
   }
 
   public void pov() {
